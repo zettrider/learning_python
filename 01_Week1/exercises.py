d8857d9ba44b42f35de1999296223b3c8bd02297
zettrider <nico@schemberg.net>
2017-03-27 18:29:40 +0200

    Week1 Challenges

--- 01_Week1/exercises.py
+++ 01_Week1/exercises.py
@@ -3,17 +3,24 @@ Task 1:
 
 1. Use the split method to divide the following IPv6 address into groups of 4 hex digits (i.e. split on the ":")
 FE80:0000:0000:0000:0101:A3EF:EE1E:1719
+
+Other possible Solution:
+ipv6Sections = ipv6.split(":")
+print(ipv6Sections)
 '''
 
 ipv6 = input("Please enter valid IPv6 Address to split:\n")
-#ipv6 = "FE80:0000:0000:0000:0101:A3EF:EE1E:1719"
+if ipv6 == "":
+    ipv6 = "FE80:0000:0000:0000:0101:A3EF:EE1E:1719"
 ipv6Split = str.split(ipv6,":")
 print(ipv6Split)
+
 
 '''
 Task 2:
 
 2. Use the join method to reunite your split IPv6 address back to its original value.
+
 '''
 
 print(":".join(ipv6Split))
@@ -24,6 +31,8 @@ Task 3:
 3. In the test3.py script above, how would you modify this to remove the trailing newline on the end of 192.168.1.1?
 '''
 
-ipv4 = "192.168.1.1"
-ipv4Split = str.split(ipv4,".")
-print(ipv4Split,end='')
+test3 = "192.168.1.1\n"
+for item in test3:
+    item.strip()
+    print(item,end="")
+
