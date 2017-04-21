a155decd2d05513a6e557526492c972002dd1fff
Nico Schemberg <nicolai.schemberg@vsa.de>
2017-04-21 12:03:17 +0200

    Possible Solution
    Still needs some beautyfying

--- 03_Week3/03_04_exercise.py
+++ 03_Week3/03_04_exercise.py
@@ -9,3 +9,40 @@ IV. Create a script that checks the validity of an IP address.  The IP address s
     For output, print the IP and whether it is valid or not.
 """
 
+import sys
+
+if len(sys.argv) > 2:
+    print('%s is not valid' % sys.argv)
+elif len(sys.argv) < 2:
+    print('%s is not valid' % sys.argv)
+else:
+    network_raw = sys.argv[1]
+
+    network_list = network_raw.split(".")
+
+    if (len(network_list)) == 4 \
+            and int(network_list[0]) >= 0 \
+            and int(network_list[0]) <=223 \
+            and (int(network_list[0]) != 127
+                 or (int(network_list[0] != 169) and int(network_list[1] != 254))) \
+            and 0 <= (int(network_list[1])) <= 255 \
+            and 0 <= (int(network_list[2])) <= 255 \
+            and 0 <= (int(network_list[3])) <= 255 :
+            #and (for i in range (1,4):
+            #    0 <= (int(network_list[i]) <= 255)):
+        print('%s is valid' % sys.argv)
+    else:
+        print('not_valid')
+
+#print(network_list[0:2])
+#print(network_list)
+
+#print(0 < (int(network_list[1]) < 255))
+
+
+'''
+for i in range (1,4):
+    print(i)
+    print(network_list[i])
+    print(0 <= (int(network_list[i])) <= 255)
+'''
